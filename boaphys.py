c26b5640e3861125789e62277638c7131ffc292f
Jan Pipek <jan.pipek@gmail.com>
2016-10-11 10:56:47 +0200

    Radiation length

--- boaphys.py
+++ boaphys.py
@@ -1,14 +1,14 @@
-def radiation_length(A, Z):
+def radiation_length(Z, A):
     """Radiation length (a.k.a. X_0)
-    
+
     The distance over which electrons lose 1/e of their energy.
-    
+
     See: https://en.wikipedia.org/wiki/Radiation_length
-    
+
     Returns
     -------
     The length in g/cm^-2
     """
-    
+
     import numpy as np
-    return 1432.8 * A / (Z * (Z + 1) * (11.319 - np.log(Z))
+    return 1432.8 * A / (Z * (Z + 1) * (11.319 - np.log(Z)))
